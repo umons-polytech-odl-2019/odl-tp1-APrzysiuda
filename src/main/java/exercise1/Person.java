ad309f38b42282e6910a3a15ede9f39488dd7e26
APrzysiuda <180517@student.umons.ac.be>
2019-10-23 15:01:33 +0200

    youpi j'ai réussi l'exercice 1 ou presque

--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,23 @@
 package exercise1;
 
 class Person {
+	private String name;
+	private int age;
+
+	Person(String nom,int monage){
+		name=nom;
+		age=monage;
+	}
+	public void setName(String nom){
+		name=nom;
+	}
+	public String getName(){
+		return name;
+	}
+	public void setAge(int monage){
+		age=monage;
+	}
+	public int getAge(){
+		return age;
+	}
 }
